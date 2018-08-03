ec712060b6b9b35d5db244dca3e320f7e379ec45
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-03 19:55:56 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,8 @@
 class CreateActors < ActiveRecord::Migration[4.2]
-  def change 
+  def change
     create_table :actors do |t|
       t.string :first_name
       t.string :last_name
-    end 
-  end 
+    end
+  end
 end 
